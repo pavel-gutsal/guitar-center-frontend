47a1420944d0268afe3ea976daebc085af9705eb
Paul Gutsal <pavel.gutsal@gmail.com>
2023-02-27 13:30:09 +0200

    feat: BrowserRouter changed for HashRouter

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { BrowserRouter } from 'react-router-dom'; // change for hash
+import { HashRouter } from 'react-router-dom'; // change for hash
 import { ThemeProvider } from 'styled-components';
 import { MUIThemeProvider } from './MUIThemeProvider';
 import { RouterContainer } from './navigation/RouterContainer';
@@ -12,12 +12,12 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <MUIThemeProvider>
-        <BrowserRouter>
+        <HashRouter>
           <QueryClientProvider client={queryClient}>
             <RouterContainer />
             {/* <ReactQueryDevtools initialIsOpen={false} /> */}
           </QueryClientProvider>
-        </BrowserRouter>
+        </HashRouter>
       </MUIThemeProvider>
     </ThemeProvider>
   );
